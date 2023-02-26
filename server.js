6d6ca0adf69d318ff7ff4367fbca6e329097dc03
anitkumarsingh <anikumarsingh99@gmail.com>
2023-02-26 17:48:59 +0530

    routing

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const http = require('http');
 
-const PORT  = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-const server = http.createServer((req,res)=>{
-  res.writeHead(200,{
-    'Content-Type':'text/html'
-  })
-  res.end('<h1>Hello Sir Issac Newton is yours friend</h1>')
-})
-server.listen(PORT,()=>{
-  console.log(`Server is running on port ${PORT}...`)
-})
+const server = http.createServer((req, res) => {
+	
+	if (req.url == '/friends') {
+    console.log(req.url);
+		res.writeHead(200, {
+			'Content-Type': 'application/json'
+		});
+		res.end(
+			JSON.stringify({
+				id: 1,
+				name: 'Sir Issac Newton'
+			})
+		);
+	} else if (req.url === '/message') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<body>');
+		res.write('<ul>');
+		res.write('<li>Hello Sir Issac Newton</li>');
+		res.write('<li>What is your idea about astronomy?</li>');
+		res.write('</ul>');
+		res.write('</body>');
+		res.write('</html>');
+		res.end();
+	}
+});
+server.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}...`);
+});
